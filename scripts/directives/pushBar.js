dc272b7537f5f34aa8cb62ce793bfb5e2d7310de
ANOSoft <beco4@hotmail.com>
2015-04-29 09:55:18 +0200

    Add Alfth_29-04-2015

---
+++ scripts/directives/pushBar.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('GMA').directive('pushBar', function ($state, menuHandler) {
+        return {
+            restrict: 'A',
+            scope: {
+                templateId: '=',
+                contentId: '='
+            },
+            link: function (scope, el) {
+                alert('directive built');
+                el.parent().on("click", function () {
+                    alert('bar clicked:' + scope.templateId + ', ' + scope.contentId);
+                    $state.go(menuHandler.getMenuItemState({ TemplateId: scope.templateId }), { id: scope.contentId });
+                });
+            }
+        };
+    });
+})();
